a01d7ef05d8bddbdf5e616f5e5e736115cbdadc8
Guilherme Domith <guilhermedomith@gmail.com>
2021-04-26 22:24:16 -0300

    Add operacoes ao Settings

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -8,6 +8,11 @@ interface ISettingsCreate {
   username: string;
 }
 
+interface ISettingsUpdate {
+  chat: boolean;
+  username: string;
+}
+
 class SettingsService {
   private settingsRepo: Repository<Setting>;
 
@@ -18,16 +23,32 @@ class SettingsService {
   async create({ chat, username }: ISettingsCreate) {
     const userAlreadyExists = await this.settingsRepo.findOne({ username })
 
-    if(userAlreadyExists) {
+    if (userAlreadyExists) {
       throw new Error("User already exists!");
     }
-    
+
     const settings = this.settingsRepo.create({
       chat,
       username
     });
 
     return await this.settingsRepo.save(settings);
+  }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepo.findOne({
+      username
+    });
+
+    return settings;
+  }
+
+  async update({ username, chat }: ISettingsUpdate) {
+    await this.settingsRepo.createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", { username })
+      .execute();
   }
 }
 
