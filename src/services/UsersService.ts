777e2ba6688898133a31f8b0c19699adc404f927
Guilherme Domith <guilhermedomith@gmail.com>
2021-04-26 22:24:44 -0300

    Add operacoes ao User

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -13,16 +13,27 @@ class UsersService {
   constructor() {
     this.usersRepo = getCustomRepository(UsersRepository);
   }
-  
-  async create({ email }: IUserCreate) {
+
+  async create({ email }: IUserCreate): Promise<User> {
     const userAlreadyExists = await this.usersRepo.findOne({ email });
 
-    if(userAlreadyExists) {
+    if (userAlreadyExists) {
       return userAlreadyExists;
     }
 
     const user = this.usersRepo.create({ email });
     return await this.usersRepo.save(user)
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    console.log(email)
+    try {
+      const user = await this.usersRepo.findOne({ email });
+      return user;
+    } catch (err) {
+      console.log(err)
+      return null
+    }
   }
 }
 
